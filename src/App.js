e049bd21cc3c1d2adfbd2c0e89fabe8176087f7b
Adam <N00211418@iadt.ie>
2023-12-05 20:35:53 +0000

    created new routes for enrole

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import LecturerShow from './pages/lecturers/Show'
 import LecturerCreate from './pages/lecturers/Create'
 import LecturerEdit from './pages/lecturers/Edit'
 
+import EnrolmentIndex from './pages/enrolments/Index'
+import EnrolmentShow from './pages/enrolments/Show'
+import EnrolmentCreate from './pages/enrolments/Create'
+import EnrolmentEdit from './pages/enrolments/Edit'
+
 import { useEffect, useState } from 'react';
 
 export const Context = React.createContext()
@@ -51,10 +56,14 @@ function App() {
             <Route path='/courses/show/:courseID' element={<CourseShow />} />
             <Route path='/courses/create' element={<CourseCreate />} />
             <Route path='/courses/edit/:courseID' element={<CourseEdit />} />
-            <Route path='lecturers' element={<LecturerIndex />} />
-            <Route path='lecturers/:lecturerID' element={<LecturerShow />} />
-            <Route path='lecturers/create' element={<LecturerCreate />} />
-            <Route path='lecturers/edit/:lecturerID' element={<LecturerEdit />} />
+            <Route path='/lecturers' element={<LecturerIndex />} />
+            <Route path='/lecturers/:lecturerID' element={<LecturerShow />} />
+            <Route path='/lecturers/create' element={<LecturerCreate />} />
+            <Route path='/lecturers/edit/:lecturerID' element={<LecturerEdit />} />
+            <Route path='/enrolments' element={<EnrolmentIndex />} />
+            <Route path='/enrolments/:enrolmentID' element={<EnrolmentShow />} />
+            <Route path='/enrolments/create' element={<EnrolmentCreate />} />
+            <Route path='/enrolments/edit/:enrolmentID' element={<EnrolmentEdit />}/>
           </Routes>
         </Router>
       </Context.Provider>
