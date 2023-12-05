63ac5bf88618907e65aa1d2df9e3d360e1269e0e
Adam <N00211418@iadt.ie>
2023-12-05 01:59:13 +0000

    added new routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 
 import CourseIndex from './pages/courses/Index'
+import CourseShow from './pages/courses/Show'
+import CourseCreate from './pages/courses/Create'
+import CourseEdit from './pages/courses/Edit'
+
 import { useEffect, useState } from 'react';
 
 export const Context = React.createContext()
+
 
 function App() {
 
@@ -33,10 +39,13 @@ function App() {
     <div className="App">
       <Context.Provider value={[isAuthenticated, onAuthenticated]}>
         <Router>
-          <NavBar />
+          { isAuthenticated ? <NavBar /> : ''}
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/courses' element={<CourseIndex />} />
+            <Route path='/courses/show/:courseID' element={<CourseShow />} />
+            <Route path='/courses/create' element={<CourseCreate />} />
+            <Route path='/courses/edit/:courseID' element={<CourseEdit />} />
           </Routes>
         </Router>
       </Context.Provider>
